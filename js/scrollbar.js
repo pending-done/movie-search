d62734e261d96538ce48b854cdbc6b9460ac89f4
789elf <789elf@naver.com>
2024-07-23 21:27:17 +0900

    스크롤바 클릭으로도 스크롤 되도록 수정, 클릭 유지

--- js/scrollbar.js
+++ js/scrollbar.js
@@ -6,16 +6,25 @@ document.addEventListener('DOMContentLoaded', function() {
     let startY;
     let startTop;
 
-    thumb.addEventListener('mousedown', function(event) {
+
+    scrollbar.addEventListener('mousedown', function(event) {
+        // mousedown => 해당 위치로 스크롤
+        const clickY = event.clientY - scrollbar.getBoundingClientRect().top;
+        const thumbTop = clickY - thumb.clientHeight / 2;
+        thumb.style.top = `${Math.max(0, Math.min(thumbTop, scrollbar.clientHeight - thumb.clientHeight))}px`;
+        const scrollPercent = thumbTop / (scrollbar.clientHeight - thumb.clientHeight);
+        window.scrollTo(0, scrollPercent * (content.scrollHeight - window.innerHeight));
+
+        // 클릭유지 된 상태로 mousemove
         isDragging = true;
         startY = event.clientY;
         startTop = thumb.offsetTop;
-        document.body.style.userSelect = 'none';  // 텍스트 선택 비활성화
+        document.body.style.userSelect = 'none';
     });
 
     document.addEventListener('mouseup', function() {
         isDragging = false;
-        document.body.style.userSelect = '';  // 텍스트 선택 활성화
+        document.body.style.userSelect = '';
     });
 
     document.addEventListener('mousemove', function(event) {
@@ -28,6 +37,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const scrollPercent = newTop / (scrollbar.clientHeight - thumb.clientHeight);
         window.scrollTo(0, scrollPercent * (content.scrollHeight - window.innerHeight));
     });
+
 
     window.addEventListener('scroll', function() {
         if (!isDragging) {
